3feb4a166f220da7d5ff4dc4965460b3b67ee407
Peter299792458c <peter.burgess10@gmail.com>
2025-09-10 23:09:04 -0400

    changed func_x/y and did calculations for earth and jupiter

--- LAB1/Question2a-2.py
+++ LAB1/Question2a-2.py
@@ -3,20 +3,20 @@ import matplotlib.pyplot as plt
 
 M_s = 1
 G = 39.5
+M_j = 0.0009543
+M_e   = 0.000003003
 
-def get_ACC(G, M_s, r):
-    return (G * M_s) / (r ** 3)
+def get_ACC(G, M, r):
+    return (G * M) / (r ** 3)
 
 def get_radius(x, y):
     return np.sqrt(x ** 2 + y ** 2)
 
+def func_x(x_i, vx_i, acc_x, dt):
+    return x_i + vx_i * dt, vx_i + acc_x * dt
 
-def func_x(x_i, vx_i, r, dt):
-    return x_i + (vx_i * dt), vx_i - (get_ACC(G, M_s, r) * x_i * dt)
-
-
-def func_y(y_i, vy_i, r, dt):
-    return y_i + (vy_i * dt), vy_i - (get_ACC(G, M_s, r) * y_i * dt)
+def func_y(y_i, vy_i, acc_y, dt):
+    return y_i + vy_i * dt, vy_i + acc_y * dt
 
 
 if __name__ == "__main__":
@@ -28,26 +28,18 @@ if __name__ == "__main__":
     total_time = 10
     n = int(total_time / dt)
 
-    # Jupiter
-    # initialize the arrays
-    x = np.zeros(n)
-    y = np.zeros(n)
-    vx = np.zeros(n)
-    vy = np.zeros(n)
+    #Jupiter initialize the arrays
+    x_j = np.zeros(n)
+    y_j = np.zeros(n)
+    vx_j = np.zeros(n)
+    vy_j = np.zeros(n)
 
-    x[0] = 5.2
-    y[0] = 0
-    vx[0] = 0
-    vy[0] = 2.63
+    x_j[0] = 5.2
+    y_j[0] = 0
+    vx_j[0] = 0
+    vy_j[0] = 2.63
 
-    for i in range(n - 1):
-        r = get_radius(x[i], y[i])
-        x[i + 1], vx[i + 1] = func_x(x[i], vx[i], r, dt)
-        y[i + 1], vy[i + 1] = func_y(y[i], vy[i], r, dt)
-
-
-    #Earth
-    # initialize the arrays
+    #Earth initialize the arrays
     x_e = np.zeros(n)
     y_e = np.zeros(n)
     vx_e = np.zeros(n)
@@ -58,13 +50,59 @@ if __name__ == "__main__":
     vx_e[0] = 0
     vy_e[0] = 6.18
 
-    for i in range(n - 1):
-        r = get_radius(x[i], y[i])
-        x_e[i + 1], vx_e[i + 1] = func_x(x_e[i], vx_e[i], r, dt)
-        y_e[i + 1], vy_e[i + 1] = func_y(y_e[i], vy_e[i], r, dt)
+    #Jupiter calculations
 
+
+    for i in range(n - 1):
+        #Jupiter–Sun
+        r_js = get_radius(x_j[i], y_j[i])
+        a_js_x = -get_ACC(G, M_s, r_js) * x_j[i]
+        a_js_y = -get_ACC(G, M_s, r_js) * y_j[i]
+        #Jupiter–Earth
+        dx_je = x_j[i] - x_e[i]
+        dy_je = y_j[i] - y_e[i]
+        r_je = get_radius(dx_je, dy_je)
+        a_je_x = -get_ACC(G, M_e, r_je) * dx_je
+        a_je_y = -get_ACC(G, M_e, r_je) * dy_je
+        #total Jupiter accel
+        a_jx = a_js_x + a_je_x
+        a_jy = a_js_y + a_je_y
+
+        #update Jupiter
+        x_j[i + 1], vx_j[i + 1] = func_x(x_j[i], vx_j[i], a_jx, dt)
+        y_j[i + 1], vy_j[i + 1] = func_y(y_j[i], vy_j[i], a_jy, dt)
+
+        #Earth calculations
+        #Earth–Sun
+        r_es = get_radius(x_e[i], y_e[i])
+        a_es_x = -get_ACC(G, M_s, r_es) * x_e[i]
+        a_es_y = -get_ACC(G, M_s, r_es) * y_e[i]
+
+        #Earth–Jupiter
+        dx_ej = x_e[i] - x_j[i]
+        dy_ej = y_e[i] - y_j[i]
+        r_ej = get_radius(dx_ej, dy_ej)
+        a_ej_x = -get_ACC(G, M_j, r_ej) * dx_ej
+        a_ej_y = -get_ACC(G, M_j, r_ej) * dy_ej
+
+        #total Earth accel
+        a_ex = a_es_x + a_ej_x
+        a_ey = a_es_y + a_ej_y
+
+        #update Earth
+        x_e[i + 1], vx_e[i + 1] = func_x(x_e[i], vx_e[i], a_ex, dt)
+        y_e[i + 1], vy_e[i + 1] = func_y(y_e[i], vy_e[i], a_ey, dt)
+
+
+    #plotting orbits
     plt.figure(1)
     plt.xlabel("x(AU)")
     plt.ylabel("y(AU)")
-    plt.plot(np.array(x), np.array(y))
+    plt.plot(x_e, y_e, label="Earth")
+    plt.plot(x_j, y_j, label="Jupiter")
+    plt.scatter(0, 0, color="gold", s=100, label="Sun")
+    plt.plot(np.array(x_e), np.array(y_e))
+    plt.plot(np.array(x_j), np.array(y_j))
+    plt.axis("equal")
+    plt.legend()
     plt.show()
