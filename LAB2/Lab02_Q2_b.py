f538536fd7dd1a4c0efc8e3cb25aae69dbf2dac3
reeshav <reeshav.kumar@mail.utoronto.ca>
2025-09-17 18:12:43 -0400

    Plots

--- LAB2/Lab02_Q2_b.py
+++ LAB2/Lab02_Q2_b.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from Lab02_Q2_a import simpson_int
 
 
@@ -13,12 +14,27 @@ def bessel_integrand(x, level):
 if __name__ == "__main__":
     N_steps = 1000
 
-    # For J_0
-    level = 0
-
     bessel_j0 = []
-    for x in range(0, 20):
-        bessel_j0.append(simpson_int(bessel_integrand(x, 0), N_steps, lower_limit=0, upper_limit=np.pi))
+    bessel_j3 = []
+    bessel_j5 = []
 
-    print(bessel_j0)
+    # x values from 0 to 20 with step 0.5
+    x_vals = np.arange(0, 20, 0.1)
 
+    for i in x_vals:
+        bessel_j0.append(simpson_int(bessel_integrand(i, 0), N_steps, lower_limit=0, upper_limit=np.pi))
+        bessel_j3.append(simpson_int(bessel_integrand(i, 3), N_steps, lower_limit=0, upper_limit=np.pi))
+        bessel_j5.append(simpson_int(bessel_integrand(i, 5), N_steps, lower_limit=0, upper_limit=np.pi))
+
+
+    # plot bessel functions
+    plt.figure(figsize=(10, 6))
+    plt.plot(x_vals, bessel_j0, label=r"$J_0(x)$", color="red")
+    plt.plot(x_vals, bessel_j3, label=r"$J_3(x)$", color="blue")
+    plt.plot(x_vals, bessel_j5, label=r"$J_5(x)$", color="green")
+
+    plt.xlabel("x")
+    plt.ylabel(r"$J_n(x)$")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
