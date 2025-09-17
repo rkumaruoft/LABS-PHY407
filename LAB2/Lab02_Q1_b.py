083f22abd039aee3564f5d7f3bd6128f736ffe91
reeshav <reeshav.kumar@mail.utoronto.ca>
2025-09-17 10:47:31 -0400

    Q1 changes

--- LAB2/Lab02_Q1_b.py
+++ LAB2/Lab02_Q1_b.py
@@ -1,11 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-array = np.loadtxt('cdata.txt')
-
-true_std = np.std(array, ddof=1)
-
-
 def method_1_std(array1):
     sum1 = 0
     for item in array1:
@@ -30,15 +25,33 @@ def method_2_std(array):
     numerator = sum_x_squared - ((sum_x ** 2) / n)
     return np.sqrt(numerator / (n - 1))
 
+def method_2_std_fixed(array):
+    n = len(array)
+    sum_x = 0
+    sum_x_squared = 0
+
+    for item in array:
+        sum_x += item
+        sum_x_squared += (item ** 2)
+
+    numerator = sum_x_squared - ((sum_x ** 2) / n)
+    return np.sqrt(numerator / (n - 1))
+
+
 def get_relative_error(value1, value2):
-    return abs(value1 - value2)/value2
+    return abs(value1 - value2) / value2
 
 
-print("method 1 std: ", method_1_std(array))
-print("method 2 std: ", method_2_std(array))
-print("true std: ", true_std)
+if __name__ == "__main__":
+    array = np.loadtxt('cdata.txt')
 
-print("relative error method 1:", get_relative_error(method_1_std(array), true_std))
-print("relative error method 2:", get_relative_error(method_2_std(array), true_std))
+    true_std = np.std(array, ddof=1)
 
-#print(array)
+    print("method 1 std: ", method_1_std(array))
+    print("method 2 std: ", method_2_std(array))
+    print("true std: ", true_std)
+
+    print("relative error method 1:", get_relative_error(method_1_std(array), true_std))
+    print("relative error method 2:", get_relative_error(method_2_std(array), true_std))
+
+# print(array)
--- LAB2/Lab02_Q1_c.py
+++ LAB2/Lab02_Q1_c.py
@@ -1,10 +1,31 @@
 import numpy as np
-from Lab02_Q1 import method_1_std
+from Lab02_Q1_b import method_1_std, method_2_std, get_relative_error
 
 if __name__ == "__main__":
     mean_1, sigma_1, n_1 = 0., 1., 2000
-    mean_2, sigma_2, n_2 = 0., 1., 2000
+    mean_2, sigma_2, n_2 = 1.e7, 1., 2000
 
     sequence_1 = np.random.normal(mean_1, sigma_1, n_1)
     sequence_2 = np.random.normal(mean_2, sigma_2, n_2)
+
+    # for sequence 1
+    std1_method_1 = method_1_std(sequence_1)
+    std1_method_2 = method_2_std(sequence_1)
+    std1_real = np.std(sequence_1, ddof=1)
+    seq1_error_1 = get_relative_error(std1_method_1, std1_real)
+    seq1_error_2 = get_relative_error(std1_method_2, std1_real)
+    print(f"Sequence 1 std real: {std1_real}")
+    print(f"Sequence 1 - Method 1 std:{std1_method_1} Error: {seq1_error_1}")
+    print(f"Sequence 1 - Method 2 std:{std1_method_2} Error: {seq1_error_2}")
+
+    # for sequence 2
+    std2_method_1 = method_1_std(sequence_2)
+    std2_method_2 = method_2_std(sequence_2)
+    std2_real = np.std(sequence_2, ddof=1)
+    seq2_error_1 = get_relative_error(std2_method_1, std2_real)
+    seq2_error_2 = get_relative_error(std2_method_2, std2_real)
+    print(f"Sequence 2 std real: {std2_real}")
+    print(f"Sequence 2 - Method 1 std:{std2_method_1} Error: {seq2_error_1}")
+    print(f"Sequence 2 - Method 2 std:{std2_method_2} Error: {seq2_error_2}")
+
 
