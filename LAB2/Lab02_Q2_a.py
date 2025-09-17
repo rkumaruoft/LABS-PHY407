216f17b556b5df175d6b1e468d7a31fcc9dd0990
Peter299792458c <peter.burgess10@gmail.com>
2025-09-17 11:28:30 -0400

    PUShhhhhhh

--- LAB2/Lab02_Q2_a.py
+++ LAB2/Lab02_Q2_a.py
@@ -12,6 +12,21 @@ def trapezoidal_int(diff_func, N_steps, lower_limit, upper_limit):
         s += f(lower_limit + k * h)
     return h*s
 
+def simpson_int(diff_func, N_steps, lower_limit, upper_limit):
+    h = (upper_limit - lower_limit) / N_steps  # width of slice
+
+    sum1 = 0
+    for k in range(1, N_steps, 2):
+        sum1 += diff_func(lower_limit + k * h)
+
+    sum2 = 0
+    for k in range(2, N_steps, 2):
+        sum2 += diff_func(lower_limit + k * h)
+
+    s = (h/3)*(diff_func(lower_limit) + diff_func(upper_limit) +
+               4*sum1 + 2*sum2)
+    return s
+
 def exact_int(diff_func, lower_limit, upper_limit):
     # sympy integration for reference
     xs = sympy.Symbol('xs', real=True)  # the variable of integration
@@ -21,6 +36,7 @@ def exact_int(diff_func, lower_limit, upper_limit):
 a = 0.0 # beginning of interval
 b = 1.0 # end of interval
 
-print("Trapezoidal rule: ", trapezoidal_int(f, N_steps = 100000, lower_limit = a, upper_limit = b))
+print("Trapezoidal rule: ", trapezoidal_int(f, N_steps = 1000, lower_limit = a, upper_limit = b))
+print("Simpsons rule: ", simpson_int(f, N_steps = 1000, lower_limit = a, upper_limit = b))
 
 print("Exact value: ", exact_int(f, a, b))
