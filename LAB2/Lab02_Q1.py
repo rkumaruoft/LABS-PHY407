41f3c39d555ee888610f7c89be2624d1b52ab47a
Peter299792458c <peter.burgess10@gmail.com>
2025-09-17 09:58:31 -0400

    stuff added

--- LAB2/Lab02_Q1.py
+++ LAB2/Lab02_Q1.py
@@ -6,9 +6,22 @@ array = np.loadtxt('cdata.txt')
 
 true_std = np.std(array, ddof=1)
 
-def fun_1():
+def method_1_std(array1):
+    sum1 = 0
+    for item in array1:
+        sum1 += item
+    array1_mean = sum1/len(array1)
+
+    print(array1_mean)
+    print(np.mean(array))
+
+    var1 = 0
+    for item in array1:
+        var1 = (item - array1_mean)**2
+    return np.sqrt((var1/(len(array1)-1)))
 
 
+print(method_1_std(array))
 
-print(true_std)
-print(array)
+#print(true_std)
+#print(array)
