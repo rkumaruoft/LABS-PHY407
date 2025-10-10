5b79a491a20ad9496b7ff27541bbc8b936a77f9b
Peter299792458c <peter.burgess10@gmail.com>
2025-10-10 16:41:02 -0400

    Q3 updated

--- LAB5/Lab05_Q3.py
+++ LAB5/Lab05_Q3.py
@@ -1,13 +1,15 @@
 from numpy import loadtxt
-from matplotlib.pyplot import contourf, xlabel, ylabel, title, colorbar
+import matplotlib.pyplot as plt
 
 SLP = loadtxt('SLP.txt')
 Longitude = loadtxt('lon.txt')
 Times = loadtxt('times.txt')
 
-contourf(Longitude, Times, SLP)
-xlabel('longitude(degrees)')
-ylabel('days since Jan. 1 2015')
-title('SLP anomaly (hPa)')
-colorbar()
+plt.contourf(Longitude, Times, SLP)
+plt.xlabel('longitude(degrees)')
+plt.ylabel('days since Jan. 1 2015')
+plt.title('SLP anomaly (hPa)')
+plt.colorbar()
+plt.show()
+
 
